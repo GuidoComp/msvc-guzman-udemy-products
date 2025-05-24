953388e77ee5f5764370f115a9059acac91082d8
Guido Agustín Compagno <guidocompagno@gmail.com>
2025-05-24 11:39:44 -0300

    initial commit

---
+++ src/test/java/com/guido/guzman/msv/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guido.guzman.msv.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
