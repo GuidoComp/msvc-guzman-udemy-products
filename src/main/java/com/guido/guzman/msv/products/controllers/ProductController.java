84a6f8e71106dd1e2294b724e40d364fc9f1279b
Guido Agustín Compagno <guidocompagno@gmail.com>
2025-06-07 18:56:40 -0300

    add exceptions for circuit breaker test in product controller, detail method

--- src/main/java/com/guido/guzman/msv/products/controllers/ProductController.java
+++ src/main/java/com/guido/guzman/msv/products/controllers/ProductController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class ProductController {
 
@@ -21,7 +23,14 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> detail(@PathVariable Long id) {
+    public ResponseEntity<?> detail(@PathVariable Long id) throws InterruptedException {
+        if (id.equals(10L)) {
+            throw new IllegalStateException("Product not found");
+        }
+        if (id.equals(7L)) {
+            TimeUnit.SECONDS.sleep(5L);
+        }
+
         return productService.findById(id)
                 .map(product -> ResponseEntity.ok(product))
                 .orElse(ResponseEntity.notFound().build());
